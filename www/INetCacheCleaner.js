f6e7e412ea2056a92168cc8de4c17c41d86a29bc
Thanik Cheowtirakul <thanikc@gmail.com>
2017-07-20 13:44:13 +0200

    added module.exports

--- www/INetCacheCleaner.js
+++ www/INetCacheCleaner.js
@@ -1,11 +1,7 @@
 var INetCacheCleaner = {
     clean: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "INetCacheCleaner", "clean");
+        cordova.exec(successCallback, errorCallback, "INetCacheCleaner", "clean", []);
     }
 }
 
-window.clean = function(callback) {
-    cordova.exec(callback, function(err) {
-        callback('Error?');
-    }, "Clean", "clean");
-};
+module.exports = INetCacheCleaner;
